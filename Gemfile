6c7e12c44716c3d04efd166ea347baad9ff8e3ab
= <duduribeiro.gba@gmail.com>
2013-05-20 19:33:15 -0300

    first commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'mysql2'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem "rspec-rails"
+  gem "thin"
+  gem "pry-meta"
+  gem "mailcatcher"
+  gem "better_errors"
+  gem "binding_of_caller"
+  gem "meta_request" 
+end
+
+group :test do
+  gem "capybara"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UniaraForum::Application.config.secret_token = 'b08e2749bd8d59d32da2e3c31d113f8a0dac83db5ec713f2a10e0dc459b8a1ef406536e0a7ffa3fa608c743c84a390e200ad5e3e2065543241043c0b84990123'
